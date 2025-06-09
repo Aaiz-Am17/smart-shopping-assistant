276c05c34ce24733f059526c525d1c8944952bcc
Aaiz Mohsin <110551583+Aaiz-Am17@users.noreply.github.com>
2025-06-09 20:03:25 +0500

    Update data_loader.py

--- smart_tv/data_loader.py
+++ smart_tv/data_loader.py
@@ -1,1 +1,14 @@
+import pandas as pd
+
+TV_file_path = r"C:\WOLF\Private\GUNI YEAR 3\ML\TELEVISION.csv"
+
+def TV_load_dataset():
+    TV_encodings = ['utf-8', 'latin-1', 'cp1252', 'iso-8859-1']
+    for TV_encoding in TV_encodings:
+        try:
+            TV_df = pd.read_csv(TV_file_path, encoding=TV_encoding)
+            return TV_df, f"File successfully read with encoding: {TV_encoding}"
+        except UnicodeDecodeError:
+            pass
+    raise ValueError("Unable to decode the file with the specified encodings.")
 
